d3c1f29ed2714f404d3c688650f6b558ab75fe99
Gustavo Rosa <gth.rosa@uol.com.br>
2019-12-27 15:22:52 -0300

    Rebasing some initial files.

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import find_packages, setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(name='opfython',
       version='1.0.0',
-      description='A Python-inspired Optimum-Path Forest',
+      description='Python-Inspired Optimum-Path Forest',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Gustavo Rosa',
       author_email='gth.rosa@uol.com.br',
       url='https://github.com/gugarosa/opfython',
-      license='MIT',
+      license='GPL-3.0',
       install_requires=['coverage>=4.5.2',
                         'numpy>=1.13.3',
                         'pandas>=0.24.1',
                         'pylint>=1.7.4',
-                        'pytest>=3.2.3',
-                        'sphinx>=1.8.5',
-                        'sphinx-rtd-theme>=0.4.3'
+                        'pytest>=3.2.3'
                        ],
       extras_require={
           'tests': ['coverage',
@@ -26,9 +29,11 @@ setup(name='opfython',
           'Intended Audience :: Developers',
           'Intended Audience :: Education',
           'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Programming Language :: Python :: 3.6',
-          'Programming Language :: PyPy :: 3.5',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: Implementation :: PyPy',
           'Topic :: Software Development :: Libraries',
           'Topic :: Software Development :: Libraries :: Python Modules'
       ],
