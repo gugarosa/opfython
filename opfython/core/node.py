2961fbe2c6022d5d101ce1cbf51991e2dce229cd
Gustavo Rosa <gth.rosa@uol.com.br>
2019-12-27 15:13:58 -0300

    Docstringing Node and Subgraph classes.

--- opfython/core/node.py
+++ opfython/core/node.py
@@ -1,22 +1,80 @@
 import numpy as np
+import opytimizer.utils.exception as e
+
 import opfython.utils.logging as l
 
 logger = l.get_logger(__name__)
 
-class Node():
-    """
-    """
 
+class Node():
+    """A Node class is used as the lowest structure level in the OPF workflow.
+
+    """
 
     def __init__(self, idx=0, label=0, features=None):
-        """
+        """Initialization method.
+
+        Args:
+            idx (int): The node's identifier.
+            label (int): The node's label.
+            features (np.array): An array of features.
+
         """
 
-        #
+        # Initially, we need to set the node's index
         self.idx = idx
 
-        #
+        # We also need to set its label
+        self.label = label
+
+        # And finally, its array of features
         self.features = features
 
-        #
-        self.label = label
+    @property
+    def idx(self):
+        """int: Node's index.
+
+        """
+
+        return self._idx
+
+    @idx.setter
+    def idx(self, idx):
+        if not isinstance(idx, int):
+            raise e.TypeError('`idx` should be an integer')
+        if idx < 0:
+            raise e.ValueError('`idx` should be >= 0')
+
+        self._idx = idx
+
+    @property
+    def label(self):
+        """int: Node's label.
+
+        """
+
+        return self._label
+
+    @label.setter
+    def label(self, label):
+        if not isinstance(label, int):
+            raise e.TypeError('`label` should be an integer')
+        if label < 0:
+            raise e.ValueError('`label` should be >= 0')
+
+        self._label = label
+
+    @property
+    def features(self):
+        """np.array: N-dimensional array of features.
+
+        """
+
+        return self._features
+
+    @features.setter
+    def features(self, features):
+        if not isinstance(features, np.ndarray):
+            raise e.TypeError('`features` should be a numpy array')
+
+        self._features = features
--- opfython/core/subgraph.py
+++ opfython/core/subgraph.py
@@ -1,20 +1,26 @@
-import numpy as np
+import opytimizer.utils.exception as e
 
 import opfython.utils.logging as l
 from opfython.core.node import Node
 
 logger = l.get_logger(__name__)
 
+
 class Subgraph():
-    """
+    """A Subgraph class is used as a collection of Nodes and the basic structure to work with OPF.
+
     """
 
     def __init__(self, data):
-        """
+        """Initialization method.
+
+        Args:
+            data (df): A pre-loaded dataframe in the OPF file format.
+
         """
 
         logger.info('Creating class: Subgraph.')
-        
+
         # Number of nodes
         self.n_nodes = 0
 
@@ -32,29 +38,97 @@ class Subgraph():
 
         logger.info('Class created.')
 
+    @property
+    def n_nodes(self):
+        """int: Number of nodes.
+
+        """
+
+        return self._n_nodes
+
+    @n_nodes.setter
+    def n_nodes(self, n_nodes):
+        if not isinstance(n_nodes, int):
+            raise e.TypeError('`n_nodes` should be an integer')
+        if n_nodes < 0:
+            raise e.ValueError('`n_nodes` should be >= 0')
+
+        self._n_nodes = n_nodes
+
+    @property
+    def n_features(self):
+        """int: Number of features.
+
+        """
+
+        return self._n_features
+
+    @n_features.setter
+    def n_features(self, n_features):
+        if not isinstance(n_features, int):
+            raise e.TypeError('`n_features` should be an integer')
+        if n_features < 0:
+            raise e.ValueError('`n_features` should be >= 0')
+
+        self._n_features = n_features
+
+    @property
+    def nodes(self):
+        """list: List of Nodes that belongs to the Subgraph.
+
+        """
+
+        return self._nodes
+
+    @nodes.setter
+    def nodes(self, nodes):
+        if not isinstance(nodes, list):
+            raise e.TypeError('`nodes` should be a list')
+
+        self._nodes = nodes
+
+    @property
+    def built(self):
+        """bool: Indicate whether the function is built.
+
+        """
+
+        return self._built
+
+    @built.setter
+    def built(self, built):
+        self._built = built
 
     def _build(self, data):
-        """
+        """This method serves as the object building process.
+
+        One can define several commands here that does not necessarily
+        needs to be on its initialization.
+
+        Args:
+            data (df): A pre-loaded dataframe in the OPF file format.
+
         """
 
         logger.debug('Running private method: build().')
-        
-        #
+
+        # Iterate over every possible sample
         for idx, label, feature in zip(data['idx'], data['labels'], data['features']):
-            #
+            # Creates a Node structure
             node = Node(idx, label, feature)
 
-            #
+            # Appends the node to the list
             self.nodes.append(node)
 
-        #
+        # Calculates the number of nodes
         self.n_nodes = len(self.nodes)
 
-        #
+        # Calculates the number of features
         self.n_features = self.nodes[0].features.shape[0]
-        
+
         # Set built variable to 'True'
         self.built = True
 
         # Logging attributes
-        logger.debug(f'Nodes: {self.n_nodes} | Features: {self.n_features} | Built: {self.built}')
+        logger.debug(
+            f'Nodes: {self.n_nodes} | Features: {self.n_features} | Built: {self.built}')
