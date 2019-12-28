31d849962d2204cc805e4c92e04db80449aadb5f
Gustavo Rosa <gth.rosa@uol.com.br>
2019-12-28 16:56:19 -0300

    Needs to fix the squared cord distance error.

--- opfython/math/distance.py
+++ opfython/math/distance.py
@@ -3,22 +3,106 @@ import numpy as np
 import opfython.utils.constants as c
 
 
-def squared_euclidean_distance(x, y):
-    """Calculates the Squared Euclidean Distance.
+def canberra_distance(x, y):
+    """Calculates the Canberra Distance.
 
     Args:
         x (np.array): N-dimensional array.
         y (np.array): N-dimensional array.
 
     Returns:
-        The Squared Euclidean Distance between x and y.
+        The Canberra Distance between x and y.
 
     """
-    
+
+    # Calculating the auxiliary term
+    aux = np.fabs(x + y)
+
+    # Gathering the conditional over the auxiliary term
+    cond = np.where(aux > 0, aux, 1)
+
+    # Calculating the canberra distance for each dimension
+    dist = np.fabs(x - y) / cond
+
+    return np.sum(dist)
+
+
+def chi_squared_distance(x, y):
+    """Calculates the Chi-Squared Distance.
+
+    Args:
+        x (np.array): N-dimensional array.
+        y (np.array): N-dimensional array.
+
+    Returns:
+        The Chi-Squared Distance between x and y.
+
+    """
+
+    # Calculating the sum on `x` array
+    f = np.sum(x)
+
+    # Calculating the sum on `y` array
+    g = np.sum(y)
+
+    # Calculating the chi-squared distance for each dimension
+    dist = 1 / (x + y + c.EPSILON) * ((x / f - y / g) ** 2)
+
+    return np.sqrt(np.sum(dist))
+
+
+def euclidean_distance(x, y):
+    """Calculates the Euclidean Distance.
+
+    Args:
+        x (np.array): N-dimensional array.
+        y (np.array): N-dimensional array.
+
+    Returns:
+        The Euclidean Distance between x and y.
+
+    """
+
     # Calculates the squared euclidean distance for each dimension
     dist = (x - y) ** 2
 
-    return np.sum(dist)
+    return np.sqrt(np.sum(dist))
+
+
+def gaussian_distance(x, y, gamma=1):
+    """Calculates the Gaussian Distance.
+
+    Args:
+        x (np.array): N-dimensional array.
+        y (np.array): N-dimensional array.
+
+    Returns:
+        The Gaussian Distance between x and y.
+
+    """
+
+    # Calculates the squared euclidean distance for each dimension
+    dist = (x - y) ** 2
+
+    return np.exp(-gamma * np.sqrt(np.sum(dist)))
+
+
+def log_euclidean_distance(x, y):
+    """Calculates the Log Euclidean Distance.
+
+    Args:
+        x (np.array): N-dimensional array.
+        y (np.array): N-dimensional array.
+
+    Returns:
+        The Log Euclidean Distance between x and y.
+
+    """
+
+    # Calculates the squared euclidean distance for each dimension
+    dist = (x - y) ** 2
+
+    return c.MAX_ARC_WEIGHT * np.log(np.sqrt(np.sum(dist)) + c.EPSILON)
 
 
 def log_squared_euclidean_distance(x, y):
@@ -32,59 +116,71 @@ def log_squared_euclidean_distance(x, y):
         The Log Squared Euclidean Distance between x and y.
 
     """
-    
+
     # Calculates the squared euclidean distance for each dimension
     dist = (x - y) ** 2
 
     return c.MAX_ARC_WEIGHT * np.log(np.sum(dist) + 1)
 
-def euclidean_distance(x, y):
-    """Calculates the Euclidean Distance.
+
+def manhattan_distance(x, y):
+    """Calculates the Manhattan Distance.
 
     Args:
         x (np.array): N-dimensional array.
         y (np.array): N-dimensional array.
 
     Returns:
-        The Euclidean Distance between x and y.
-        
+        The Manhattan Distance between x and y.
+
     """
-    
-    # Calculates the squared euclidean distance for each dimension
-    dist = (x - y) ** 2
 
-    return np.sqrt(np.sum(dist))
+    # Calculates the manhattan distance for each dimension
+    dist = np.fabs(x - y)
 
-def log_euclidean_distance(x, y):
-    """Calculates the Log Euclidean Distance.
+    return np.sum(dist)
+
+
+def squared_cord_distance(x, y):
+    """Calculates the Squared-Cord Distance.
 
     Args:
         x (np.array): N-dimensional array.
         y (np.array): N-dimensional array.
 
     Returns:
-        The Log Euclidean Distance between x and y.
-        
+        The Squared-Cord Distance between x and y.
+
     """
-    
-    # Calculates the squared euclidean distance for each dimension
-    dist = (x - y) ** 2
 
-    return c.MAX_ARC_WEIGHT * np.log(np.sqrt(np.sum(dist)) + c.EPSILON)
+    # Calculating the first auxiliary term
+    aux1 = np.sqrt(x)
 
-def gaussian_distance(x, y, gamma=1):
-    """Calculates the Gaussian Distance.
+    # Calculating the second auxiliary term
+    aux2 = np.sqrt(y)
+
+    # Gathering the conditional over the auxiliary term
+    cond = np.where(aux1 >= 0 and aux2 >= 0, aux1 - aux2, 0)
+
+    # Calculating the squared-cord distance for each dimension
+    dist = cond ** 2
+
+    return np.sum(dist)
+
+
+def squared_euclidean_distance(x, y):
+    """Calculates the Squared Euclidean Distance.
 
     Args:
         x (np.array): N-dimensional array.
         y (np.array): N-dimensional array.
 
     Returns:
-        The Gaussian Distance between x and y.
-        
+        The Squared Euclidean Distance between x and y.
+
     """
-    
+
     # Calculates the squared euclidean distance for each dimension
     dist = (x - y) ** 2
 
-    return np.exp(-gamma * np.sqrt(np.sum(dist)))
+    return np.sum(dist)
--- test.py
+++ test.py
@@ -23,5 +23,5 @@ import opfython.math.distance as d
 x = np.asarray([2, 3, 4, 5])
 y = np.asarray([1, 2, 3, 1])
 
-dist = d.euclidean_distance(x, y)
+dist = d.squared_cord_distance(x, y)
 print(dist)
