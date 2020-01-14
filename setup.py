bbb3206b3eada5b67d2b8dbd14ad9c350e2fd3ce
Gustavo Rosa <gth.rosa@uol.com.br>
2020-01-14 12:17:47 -0300

    Bumping up version for further release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='opfython',
-      version='1.0.1',
+      version='1.0.2',
       description='Python-Inspired Optimum-Path Forest',
       long_description=long_description,
       long_description_content_type='text/markdown',
