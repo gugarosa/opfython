a4d179c7359bba685b7e84d715f39228073bb824
Gustavo Rosa <gth.rosa@uol.com.br>
2020-07-07 18:12:22 -0300

    Fixing up pre_compute_distances example.

--- examples/math/pre_compute_distances.py
+++ examples/math/pre_compute_distances.py
@@ -9,10 +9,5 @@ txt = l.load_txt('data/boat.txt')
 # Parsing a pre-loaded numpy array
 X, Y = p.parse_loader(txt)
 
-# Splitting data into training and testing sets
-X_train, X_test, Y_train, Y_test = s.split(
-    X, Y, percentage=0.5, random_state=1)
-
 # Creating a file of pre-computed distances
-g.pre_compute_distance(X_train, 'boat_split_distances.txt',
-                       distance='log_squared_euclidean')
+g.pre_compute_distance(X, 'boat_split_distances.txt', distance='log_squared_euclidean')
